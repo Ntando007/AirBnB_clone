b8b20ba043abeffbf9e1a82c257977357b2bd8e6
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-02-14 15:14:51 +0200

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        __tablename__: Linked table for the Amenity class
+        name: input name
+        place_amenities: sqlalchemy relationship
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
